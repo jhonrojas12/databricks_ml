7426e0612ffbec72ed8cf2f3e038c583fb1baeb4
Jhon Rojas <jerf_961030@hotmail.com>
2026-02-01 21:29:30 -0500

    adding pytests

---
+++ tests/test_math_utils.py
@@ -0,0 +1,11 @@
+from utils.math_utils import suma, resta
+
+
+def test_suma():
+    assert suma(2, 3) == 5
+    assert suma(-1, 1) == 0
+
+
+def test_resta():
+    assert resta(5, 3) == 2
+    assert resta(0, 5) == -5
